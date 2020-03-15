77fcb401ceacfa03f4aaa3dccfb4628f8604dabb
Júlia Dias <juliia.dias00@gmail.com>
2020-03-15 20:35:57 -0300

    Fim das funções de jogo
    Yeetz

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -7,8 +7,11 @@ import { StyledTetris } from './styles/StyledTetris';
 import { StyledTetrisWrapper } from './styles/StyledTetris';
 
 //Custom Hooks
-import { usePlayer } from '../hooks/usePlayer'
-import { useStage } from '../hooks/useStage'
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+import {useInterval} from '../hooks/useInterval';
+import {useGameStatus} from '../hooks/useGameStatus';
+
 //components
 import Display from './Display';
 import Stage from './Stage';
@@ -19,8 +22,8 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false);
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-    const [stage, setStage] = useStage(player, resetPlayer); 
-
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer); 
+    const [ score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
     console.log('renderizando')
 
@@ -33,11 +36,21 @@ const Tetris = () => {
     const startGame = () => {
         //reiniciar
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
+        setScore(0);
+        setRows(0);
+        setLevel(0);
         setGameOver(false);
+
     }
 
     const drop = () => {
+        //Sobe de nivel a cada 10 colunas limpas
+        if(rows > (level + 1)*10){
+            setLevel(prev => prev + 1);
+            setDropTime(1000/(level+1) + 200)
+        }
         if (!checkCollision(player, stage, {x:0, y:1})){
             updatePlayerPos({x:0, y:1, collided: false})
         } else {
@@ -50,7 +63,16 @@ const Tetris = () => {
     }
 
     const dropPlayer = () => {
+        setDropTime(null)
         drop();
+    }
+
+    const keyUp = ({keyCode}) => {
+        if (!gameOver) {
+            if (keyCode === 40 || keyCode === 83) {
+                setDropTime(1000/(level+1) + 200)
+            }
+        }
     }
 
     const move = ({keyCode}) => {
@@ -68,18 +90,31 @@ const Tetris = () => {
             console.log(keyCode)
     }
 
+    //Função achada na insternet que está nos hooks (useInterval)
+    //Recebe uma função para realizar e um tempo de repetição
+    useInterval(()=> {
+        drop();
+    }, dropTime)
+
     return (
-        <StyledTetrisWrapper role='button' tabIndex="0" onKeyDown={e => move(e)}>
+        <StyledTetrisWrapper 
+        role='button' 
+        tabIndex="0" 
+        onKeyDown={e => move(e)} 
+        onKeyUp={keyUp}>
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
                     {gameOver ? (
+                        <div>
                         <Display gameOver={gameOver} text="game Over" />
+                        <Display gameOver={gameOver} text="Mais sorte da próxima vez" />
+                        </div>
                     ) : (
                             <div>
-                                <Display text="Score" />
-                                <Display text="Rows" />
-                                <Display text="Level" />
+                                <Display text={`Score ${score}`} />
+                                <Display text={`Rows ${rows}`} />
+                                <Display text={`Level ${level}`} />
                             </div>
                         )}
                     <StartButton callback={startGame}/>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,25 @@ import {createStage} from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
     const [stage, setStage] = useState(createStage());
+    const [rowsCleared, setRowsCleared] = useState(0);
 
     useEffect(()=>{
+      setRowsCleared(0);
+
+      const sweepRows = newStage =>
+      newStage.reduce((ack, row) => {
+        if(row.findIndex(cell => cell[0] ===0) === -1) {
+          setRowsCleared(prev => prev +1);
+          //O método unshift Adiciona um ou mais elementos no início de um array e retorna o número de elementos (propriedade length) atualizado.
+          //vai acrescentar o número de colunas deletadas ao topo do campo do jogo
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          //Dá a impressão de que as colunas restantes desceram
+          return ack
+        }
+        ack.push(row);
+        return ack;
+      }, []);
+
         const updateStage = prevStage =>{
             const newStage = prevStage.map(row =>
                 row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -25,6 +42,7 @@ export const useStage = (player, resetPlayer) => {
               //Verificar colisões
               if(player.collided) {
                 resetPlayer();
+                return sweepRows(newStage);
               }
               return newStage;
             };
@@ -34,5 +52,5 @@ export const useStage = (player, resetPlayer) => {
     }, [ player,
     resetPlayer,])
 
-    return[stage, setStage]
+    return[stage, setStage, rowsCleared]
 }
