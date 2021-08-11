0016da70173aa9c8580561c57c2a22e004a0c3a7
Júlia <julia@orditi.com>
2021-08-11 19:59:46 -0300

    Updates

---
+++ precache-manifest.ac07a39c69c54c6cd07821fb34f7e1aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a75ff62daacdc0da083965ce11191c2",
+    "url": "/tetroritos/index.html"
+  },
+  {
+    "revision": "a465cb82477646e3344b",
+    "url": "/tetroritos/static/css/main.b505370d.chunk.css"
+  },
+  {
+    "revision": "bdadf336107a41dcd736",
+    "url": "/tetroritos/static/js/2.18d6e5c4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/tetroritos/static/js/2.18d6e5c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a465cb82477646e3344b",
+    "url": "/tetroritos/static/js/main.90cd7794.chunk.js"
+  },
+  {
+    "revision": "7911417f162de2c4faff",
+    "url": "/tetroritos/static/js/runtime-main.4bace3ec.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetroritos/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetroritos/static/media/bg.2810fe6b.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tetroritos/static/media/logo.ee7cd8ed.svg"
+  }
+]);
